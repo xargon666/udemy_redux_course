7e46d5d533cab2a259d77596405dd30372ffeb51
Chris Pucknell <chris.pucknell@gmail.com>
2024-03-02 13:34:51 +0000

    update for lodash, add notes

--- Redux Resources 2024/redux-starter/src/index.ts
+++ Redux Resources 2024/redux-starter/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Redux Starter Project!!");
+console.log("Redux Starter Project!11!");
+
+let outputColor:string = 'background: #222; color: #bada55; font-size: 20px'
+
+import {compose, pipe} from "lodash/fp"
+
+let username = "        Chris   "
+
+const trim = (name: string) => name.trim()
+const generateMessage = (name: string) => `Hello ${name}, Good Morning!!!`
+const convertToUpper = (name: string) => name.toUpperCase()
+
+let outputFunction = pipe(trim,generateMessage,convertToUpper)
+console.log(`%c ${outputFunction(username)}`, outputColor)
